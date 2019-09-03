d0b863865f18a02e22dec128342ccd9e266dde37
Christian Edge <christian.m.edge@gmail.com>
2019-09-03 13:44:59 -0400

    Add files via upload

---
+++ CIS 216/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Author: Christian Edge
+ * Course: CIS 216 C# and .NET
+ * Week 1 Homework
+ * HelloWorld Project
+ */
+
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("I have written some code in C# before.");
+    }
+}
