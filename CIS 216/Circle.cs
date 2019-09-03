b13447da9a7bf447f93b7472714242eaabf793ac
Christian Edge <christian.m.edge@gmail.com>
2019-09-03 14:12:27 -0400

    Add files via upload

---
+++ CIS 216/Circle.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Author: Christian Edge
+ * Course: CIS 216 C# & .NET
+ * Project: Circle Project
+ * Date: 03 March 2019
+ */
+
+/* 
+ * The purpose of this program is for a user to input the radius of a circle and
+ * get back measurements about the circle: radius, diameter, circumference, and 
+ * area. The program will also construct its own circle and display its 
+ * measurements to the user.
+ */
+
+using System;
+
+
+class Circle
+{
+    //Declare instance variable
+    private float radius;
+
+
+    //Constructor without parameters
+    public Circle()
+    {
+        radius = 0;
+    }
+
+    //Constructor with one parameter
+    public Circle(float r)
+    {
+        radius = r;
+    }
+
+
+    //Declare property (with accessor methods) for "radius"
+    public float Radius
+    {
+        get
+        {
+            return radius;
+        }
+
+        set
+        {
+            radius = value;
+        }
+    }
+
+    //Declare accessor methods
+    public float getRadius()
+    {
+        return radius;
+    }
+
+    public float getDiameter()
+    {
+        return radius * 2.00f;
+    }
+
+    public float getCircumference()
+    {
+        return radius * 2.00f * 3.14159f;
+    }
+
+    public float getArea()
+    {
+        return (float)Math.Pow(radius, 2) * 3.14159f;
+    }
+
+}//End of class Circle
+
---
+++ CIS 216/CircleTest.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Author: Christian Edge
+ * Course: CIS 216 C# & .NET
+ * Project: Circle Project
+ * Date: 03 March 2019
+ */
+
+/* 
+ * The purpose of this program is for a user to input the radius of a circle and
+ * get back measurements about the circle: radius, diameter, circumference, and 
+ * area. The program will also construct its own circle and display its 
+ * measurements to the user.
+ */
+
+using System;
+
+
+class CircleTest
+{
+
+    static void Main()
+    {
+        //instantiate new objects
+        Circle userCircle = new Circle(); //Constructor no parameters
+        Circle myCircle = new Circle(7); //Constructor with a parameter
+
+        //Output instructions to user
+        Console.WriteLine("Enter a radius for your circle: ");
+
+        /* Accept user input.
+         * Pass the user-defined value to their Circle object (userCircle) via 
+         * Radius property.
+         */
+        userCircle.Radius = float.Parse(Console.ReadLine());
+
+
+        //Call the get methods, output the values to the user
+        Console.WriteLine($"The radius of your circle is {userCircle.getRadius()}");
+        Console.WriteLine($"The diameter of your circle is " +
+            $"{userCircle.getDiameter()}");
+        Console.WriteLine($"The circumference of your circle is " +
+            $"{userCircle.getCircumference()}");
+        Console.WriteLine($"The area of your circle is {userCircle.getArea()}");
+        Console.WriteLine();
+        Console.WriteLine($"The radius of my circle is {myCircle.getRadius()}");
+        Console.WriteLine($"The diameter of my circle is {myCircle.getDiameter()}");
+        Console.WriteLine($"The circumference of my circle is " +
+            $"{myCircle.getCircumference()}");
+        Console.WriteLine($"The area of my circle is {myCircle.getArea()}");
+        Console.WriteLine();
+
+    }//End of Main()
+
+}//End of class CircleTest
