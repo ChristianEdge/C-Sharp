0fb362f17364e4956668e02fd72f607e8625081a
Christian Edge <christian.m.edge@gmail.com>
2019-09-03 14:37:58 -0400

    Add files via upload

---
+++ CIS 216/Point.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * Author: Christian Edge
+ * Date: 03 March 2019
+ * Project: Point Project
+ * Course: CIS 216 C# & .NET
+ */
+
+/* The purpose of this program is for a user to provide X and Y coordinates for a
+ * point, and to output a string that specifies which quadrant that point exists in,
+ * based on the Cartesian coordinate system. The program will also output a quadrant 
+ * location for a program-defined point.
+ */
+
+using System;
+
+class Point
+{
+    //declare instance variables, members #1 & #2
+    private double x;
+    private double y;
+
+
+
+    //no parameters constructor, member #3
+    public Point()
+    {
+        //Use setters setX() & setY()
+    }
+
+
+
+    //two parameters constructor, member #4
+    //use this to pass user-based arguments
+    public Point (double X, double Y)
+    {
+        x = X;
+        y = Y;
+    }
+
+
+    
+    //mutator methods, members #5 & #6
+    public void setX(double X)
+    {
+        x = X;
+    }
+
+    public void setY(double Y)
+    {
+        y = Y;
+    }
+    
+
+
+    //accessor methods, members #7 & #8
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+
+
+    /* This method gets the quadrant location of the point
+     * Assume neither coord X nor coord Y will be equal to zero,
+     * except when they are both equal to 0.
+     * getLocation() must return a string.
+     * This is member #9.
+     */
+    public string getLocation()
+    {
+        //Nullable int to have value returned in output string
+        int? quadrant = null;
+
+        string statement;
+
+        if (x > 0 && y > 0)
+        {
+            quadrant = 1;
+        }
+
+        else if (x < 0 && y > 0)
+        {
+            quadrant = 2;
+        }
+
+        else if (x < 0 && y < 0)
+        {
+            quadrant = 3;
+        }
+
+        else if (x > 0 && y < 0)
+        {
+            quadrant = 4;
+        }
+
+        else if (x == 0 && y == 0)
+        {
+            return " point is located at origin (0, 0)";
+        }
+
+        statement = $" point is located in quadrant {quadrant}";
+
+        return statement;
+
+    }//end of getLocation()
+
+}//End of class Point
+
---
+++ CIS 216/PointTest.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Author: Christian Edge
+ * Date: 03 March 2019
+ * Project: Point Project
+ * Course: CIS 216 C# & .NET
+ */
+
+/* The purpose of this program is for a user to provide X and Y coordinates for a
+ * point, and to output a string that specifies which quadrant that point exists in,
+ * based on the Cartesian coordinate system. The program will also output a quadrant 
+ * location for a program-defined point. The users' point is named "Ginger" and the
+ * program's point is named "Fred". 
+ */
+
+using System;
+
+class PointTest
+{
+    static void Main()
+    {
+        //Output instructions
+        Console.WriteLine("Welcom to the Cartesian Coordinate Quadrant Finder " + 
+            "program. \n" + "You enter coordinates for a point and discover which " +
+            "quadrant it is located in. \n" + "Your point is named 'Ginger'. \n" +
+            "The program will generate its own point named 'Fred'. \n" + 
+            "Please use real number values for your coordinates. (Ex: -6, 4)");
+        Console.WriteLine();
+
+
+        //Accept user input and assign to variables
+        Console.WriteLine("Please enter the X value for your coordinate: ");
+        double userX = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter the Y value for your coordinate: ");
+        double userY = double.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+
+
+        //instantiate new objects
+        Point Fred = new Point();
+        Point Ginger = new Point(userX, userY);
+
+
+
+        /* Instantiate random object to generate random values for program-generated
+         * X and Y coordinates */
+        Random random = new Random();
+
+        //Use mutator methods to assign value to program-generated point
+        Fred.setX(random.Next(-10, 10));
+        Fred.setY(random.Next(-10, 10));
+
+
+
+        //Call getLocation() on the objects and output data 
+        Console.WriteLine("Ginger's coordinates are X: {0}, Y: {1}", userX, userY);
+        Console.WriteLine("Your" + Ginger.getLocation());
+        Console.WriteLine();
+        Console.WriteLine("Fred's coordinates are X: {0}, Y: {1}", Fred.getX(), Fred.getY());
+        Console.WriteLine("The program-generated" + Fred.getLocation());
+        
+    }
+
+}//End of class PointTest
+
